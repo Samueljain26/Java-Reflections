cb63d6e012803622335aa54128a6b89c81ed98e8
Samueljain26 <samueljain77@gmail.com>
2025-04-08 09:23:51 +0530

    [Samuel] ADD: Student class.

---
+++ src/main/java/com/bridgelabz/Student.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public class Student {
+    String name;
+    int age;
+
+    Student(){
+        name="dfkjaksl";
+        age=10;
+    }
+    Student(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
+}
